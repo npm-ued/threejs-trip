93744dc6b57d120b4cd1e098ab2626ae51f4bb8d
npm-ued <xiaoxiaoxcx@hotmail.com>
2021-01-06 09:00:17 +0800

    各类几何体

--- three/geometries/geometries.ts
+++ three/geometries/geometries.ts
@@ -27,8 +27,8 @@
     camera.lookAt(new THREE.Vector3(-10, 0, 0));
 
     // add subtle ambient lighting
-    const ambientLight = new THREE.AmbientLight(0x090909);
-    scene.add(ambientLight);
+    // const ambientLight = new THREE.AmbientLight(0x090909);
+    // scene.add(ambientLight);
 
     // add spotlight for the shadows
     const spotLight = new THREE.SpotLight(0xffffff);
@@ -41,13 +41,15 @@
 
     function addGeometries(scene:THREE.Scene) {
       const geoms = [];
+      // 圆柱缓冲几何体
       geoms.push(new THREE.CylinderBufferGeometry(1, 4, 4));
-      // basic cube
-      geoms.push(new THREE.BoxGeometry(2, 2, 2));
-      // basic sphere
+      // 立方体
+      geoms.push(new THREE.BoxGeometry(3, 3, 4));
+      // 球体
       geoms.push(new THREE.SphereGeometry(2));
+      // 十二面几何体
       geoms.push(new THREE.IcosahedronBufferGeometry(4));
-
+      // 顶点
       const ponits = [
         new THREE.Vector3(2, 2, 2),
         new THREE.Vector3(2, 2, -2),
@@ -68,36 +70,32 @@
       }
       // geoms.push(new THREE.LatheGeometry((pts as THREE.Vector3:[]), 12));
 
-      // create a OctahedronGeometry
+      // create a OctahedronGeometry（八面体）
       geoms.push(new THREE.OctahedronGeometry(3));
 
       // create a geometry based on a function
       // geoms.push(new THREE.ParametricGeometry(THREE.ParametricGeometries.mobius3d, 20, 10));
 
-      //
+      // 四面体
       geoms.push(new THREE.TetrahedronGeometry(3));
-
+      // 圆环几何体
       geoms.push(new THREE.TorusGeometry(3, 1, 10, 10));
-
+      // 圆环扭结几何体
       geoms.push(new THREE.TorusKnotGeometry(3, 0.5, 50, 20));
+
       let j = 0;
       for (var i = 0; i < geoms.length; i++) {
-        const cubeMaterial = new THREE.MeshLambertMaterial({wireframe: true, color: Math.random() * 0xffffff});
+        const cubeMaterial = new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff});
         const materials = [
-            new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff}),
-            new THREE.MeshBasicMaterial({color: 0x000000, wireframe: true})
+          new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff}),
+          new THREE.MeshBasicMaterial({color: 0x000000, wireframe: true})
         ];
-        // const mesh = THREE.SceneUtils.createMultiMaterialObject(geoms[i] as THREE.Geometry, materials);
-        // mesh.traverse(function (e) {
-        //     e.castShadow = true
-        // });
 
         const mesh = new THREE.Mesh(geoms[i], materials[i]);
         mesh.castShadow=true;
         mesh.position.x = -24 + ((i % 4) * 12);
         mesh.position.y = 4;
         mesh.position.z = -8 + (j * 12);
-
         if ((i + 1) % 4 == 0) {j++;}
         scene.add(mesh);
       }
